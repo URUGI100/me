0e14b6a8e75df44d92025ce63bccc87344b2519f
urugi <123052212+URUGI100@users.noreply.github.com>
2023-01-20 03:25:17 +0900

    Add files via upload

---
+++ data/system/KeyConfig.js
@@ -0,0 +1,32 @@
+var __tyrano_key_config = {
+	"key": {
+		"8": "menu",
+		"13": "next",
+		"17": "skip",
+		"27": "menu",
+		"32": "next"
+	},
+	"mouse": {
+		"right": "",
+		"center": "",
+		"wheel_up": "",
+		"wheel_down": ""
+	},
+	"gesture": {
+		"swipe_up_1": {
+			"action": "backlog"
+		},
+		"swipe_left_1": {
+			"action": "auto"
+		},
+		"swipe_right_1": {
+			"action": "menu"
+		},
+		"swipe_down_1": {
+			"action": "load"
+		},
+		"hold": {
+			"action": "skip"
+		}
+	}
+}
